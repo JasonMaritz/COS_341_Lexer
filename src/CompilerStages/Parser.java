7691b9fcbf6eaf8b0edd4753e08610cd79747086
Jason <u19053292@tuks.co.za>
2021-04-27 13:39:42 +0200

    Added empty parse functions and called them to assign SyntaxTreeHead

--- src/CompilerStages/Parser.java
+++ src/CompilerStages/Parser.java
@@ -14,7 +14,7 @@ public class Parser {
 
     public void parse() {
         //recursive descent
-        
+        SyntaxTreeRoot = parsePROGPRIME();
     }
 
     public SyntaxNode getOutput(){
@@ -23,5 +23,51 @@ public class Parser {
 
     public String toString(){
         return "Back to doing fuck-all";
+    }
+
+    private SyntaxNode parsePROGPRIME(){
+        return null;
+    }
+    private SyntaxNode parsePROG(){
+        return null;
+    }
+    private SyntaxNode parseCODE(){
+        return null;
+    }
+    private SyntaxNode parsePROC_DEFS(){
+        return null;
+    }
+    private SyntaxNode parsePROC(){
+        return null;
+    }
+    private SyntaxNode parseINSTR(){
+        return null;
+    }
+    private SyntaxNode parseIO(){
+        return null;
+    }
+    private SyntaxNode parseCALL(){
+        return null;
+    }
+    private SyntaxNode parseVAR(){
+        return null;
+    }
+    private SyntaxNode parseASSIGN(){
+        return null;
+    }
+    private SyntaxNode parseNUMEXPR(){
+        return null;
+    }
+    private SyntaxNode parseCALC(){
+        return null;
+    }
+    private SyntaxNode parseBRANCH(){
+        return null;
+    }
+    private SyntaxNode parseLOOP(){
+        return null;
+    }
+    private SyntaxNode parseBOOL(){
+        return null;
     }
 }
