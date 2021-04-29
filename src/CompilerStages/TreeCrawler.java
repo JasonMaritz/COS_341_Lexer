3e18ea119a2ac5f292e537ef1a75eb791d600763
Jason <u19053292@tuks.co.za>
2021-04-29 12:52:09 +0200

    Finished Scoping logic

--- src/CompilerStages/TreeCrawler.java
+++ src/CompilerStages/TreeCrawler.java
@@ -13,14 +13,29 @@ public class TreeCrawler {
         treeRoot = root;
     }
     public void scopeCrawl(){
+        usedScopes.add("0");
         scopeCrawl(treeRoot, "0");
     }
 
     private void scopeCrawl(SyntaxNode curr, String parentScope){
+        if(curr == null){
+            return;
+        }
         if(curr.getNodeType() == SyntaxNode.type.NONTERMINAL){
             //add scope but dont return to allow children to be scoped as well
+            if(curr.getData("symbol").equals("LOOP")||curr.getData("symbol").equals("PROC")){
+                    int i = 1;
+                    while(usedScopes.contains(parentScope+"."+i)){
+                        i++;
+                    }
+                    curr.addScope(parentScope+"."+i);
+                    usedScopes.add(parentScope+"."+i);
+            }else{
+                curr.addScope(parentScope);
+            }
         }else{
             //add scope then return to previous frame
+            curr.addScope(parentScope);
             return;
         }
         //recur through all children adding scope as you go
--- src/Nodes/SyntaxNode.java
+++ src/Nodes/SyntaxNode.java
@@ -34,7 +34,7 @@ public class SyntaxNode {
         StringBuilder res = new StringBuilder();
         res.append("\t".repeat(Math.max(0, i)));
         int k = ++i;
-        res.append(nodeType.name()).append(": ").append(data.get("symbol")).append(" Scope: ").append(data.get("scope")).
+        res.append(nodeType.name()).append(": ").append(data.get("symbol")).append(", Scope: ").append(data.get("scope")).
                 append("\n");
         for(SyntaxNode n: children){
             if(n!=null) {
